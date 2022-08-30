f981d09812fbd8fc13f2fc7834670014231310c6
amit kumar diggi <amitkumardiggi05@gmail.com>
2022-08-30 15:05:22 +0530

    initial commit for calculator functionality with gitignore

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,15 @@
+public class Calculator {
+
+    public static int divisionOfNumbers(int num1, int num2) {
+        return num1 / num2;
+    }
+
+    public int addTwoNumbers(int num1, int num2){
+        return num1+num2;
+    }
+
+    public int multiplyTwoNumbers(int num1, int num2){
+        return num1 * num2;
+    }
+
+}
